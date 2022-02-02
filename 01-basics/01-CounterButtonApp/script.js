55b1c21c394d19efa11de3bef9f04f36205d93a9
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-02-02 20:38:55 +0300

    Solve counterButton & VueCalculator

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,19 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const CounterButtonApp = {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    doCount(event) {
+      console.log(event.currentTarget);
+      this.counter += 1;
+    },
+  },
+};
+
+createApp(CounterButtonApp).mount('#app');
+/* задать вопрос про v-bind */
--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const VueCalculator = {
+  data() {
+    return {
+      actionPicked: '',
+      firstNumber: 0,
+      secondNumber: 0,
+      result: 0,
+    };
+  },
+  computed: {
+    output() {
+      switch (this.actionPicked) {
+        case 'sum':
+          this.result = this.firstNumber + this.secondNumber;
+          break;
+        case 'subtract':
+          this.result = this.firstNumber - this.secondNumber;
+          break;
+        case 'multiply':
+          this.result = this.firstNumber * this.secondNumber;
+          break;
+        case 'divide':
+          this.result = this.firstNumber / this.secondNumber;
+          break;
+      }
+      return this.result;
+    },
+  },
+};
+
+createApp(VueCalculator).mount('#app');
